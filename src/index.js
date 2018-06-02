a553e814bb999cf6714097139cf782044a36f3dd
Kévin Gossé <gossekevin1@gmail.com>
2018-06-02 08:01:04 +0200

    Add themes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Root from './layouts/app';
 import App from './App';
 import {rootStore} from './stores';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App rootStore={rootStore}/>, document.getElementById('root'));
+ReactDOM.render(<Root><App rootStore={rootStore}/></Root>, document.getElementById('root'));
 registerServiceWorker();
